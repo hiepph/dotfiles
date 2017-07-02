42e9e01e0f25c53aaa1bd1ee0895f12e92b0f9db
hiepph <hoanghiepjp96@gmail.com>
2017-07-03 03:30:09 +0700

    awesome: Config theme

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -8,17 +8,17 @@
 
 -- {{{ Main
 theme = {}
-theme.wallpaper = "~/.config/wallpapers/devil.png"
+theme.wallpaper_cmd = { "feh --bg-fill ~/.config/wallpapers/think.png" }
 -- }}}
 
 -- {{{ Styles
-theme.font      = "sans 8"
+theme.font      = "Fira Code 9"
 
 -- {{{ Colors
 theme.fg_normal  = "#DCDCCC"
 theme.fg_focus   = "#F0DFAF"
 theme.fg_urgent  = "#CC9393"
-theme.bg_normal  = "#3F3F3F"
+theme.bg_normal  = "#151618"
 theme.bg_focus   = "#1E2320"
 theme.bg_urgent  = "#3F3F3F"
 theme.bg_systray = theme.bg_normal
