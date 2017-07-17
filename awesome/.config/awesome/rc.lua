134abda46c85da099204f209ada8ff42e1490b93
hiepph <hoanghiepjp96@gmail.com>
2017-07-17 22:37:39 +0700

    awesome: Default editor to vim

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -44,7 +44,7 @@ beautiful.init("/home/hiepph/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
-editor = os.getenv("EDITOR") or "nano"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
