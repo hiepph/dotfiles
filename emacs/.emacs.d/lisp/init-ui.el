03fc8ee546818ec0a39ccb8f17a0d014a5d36377
hiepph <hoanghiepjp96@gmail.com>
2017-11-23 13:28:36 +0700

    emacs: Smart neotree

--- emacs/.emacs.d/lisp/init-ui.el
+++ emacs/.emacs.d/lisp/init-ui.el
@@ -42,5 +42,9 @@
 (setq inhibit-compacting-font-caches t)
 ;; set icons theme
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+;; Every time when the neotree window is opened, let it find current file and jump to node
+(setq neo-smart-open t)
+;; When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically
+(setq projectile-switch-project-action 'neotree-projectile-action)
 
 (provide 'init-ui)
