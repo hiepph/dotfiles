b007906ee7306b8d882bddef3f372d707d0f1da0
hiepph <hoanghiepjp96@gmail.com>
2017-11-24 16:48:45 +0700

    emacs: Add hook with Git-gutter & Magit

--- emacs/.emacs.d/lisp/init-helper.el
+++ emacs/.emacs.d/lisp/init-helper.el
@@ -4,7 +4,10 @@
 (require-package 'magit)
 
 ; Magit
+;; git status
 (global-set-key (kbd "C-x g") 'magit-status)
+;; shortcuts help
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 ; Git Gutter
 ;; Enable git-gutter on all mode
@@ -12,5 +15,9 @@
 
 ;; If you would like to use git-gutter.el and linum-mode
 (git-gutter:linum-setup)
+
+;; reload after magit
+(add-hook 'magit-post-refresh-hook
+          #'git-gutter:update-all-windows)
 
 (provide 'init-helper)
