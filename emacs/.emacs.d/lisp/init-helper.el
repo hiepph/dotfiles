158f7101a3c3cfeb84a011123287d5f1d53af74d
hiepph <hoanghiepjp96@gmail.com>
2017-11-24 17:02:37 +0700

    emacs: Switch git-gutter to git-gutter+
    Fix integration with magit

--- emacs/.emacs.d/lisp/init-helper.el
+++ emacs/.emacs.d/lisp/init-helper.el
@@ -1,6 +1,6 @@
 (require 'init-elpa)
 
-(require-package 'git-gutter)
+(require-package 'git-gutter+)
 (require-package 'magit)
 
 ; Magit
@@ -9,40 +9,11 @@
 ;; shortcuts help
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
-; Git Gutter
+; Git Gutter (+)
 ;; Enable git-gutter on all mode
-(global-git-gutter-mode +1)
+(global-git-gutter+-mode)
 
 ;; If you would like to use git-gutter.el and linum-mode
 (git-gutter:linum-setup)
-
-;; reload after magit
-(defvar my-magit-after-stage-hooks nil
-  "Hooks to be run after staging one item in magit.")
-
-(defvar my-magit-after-unstage-hooks nil
-  "Hooks to be run after unstaging one item in magit.")
-
-(defadvice magit-stage-item (after run-my-after-stage-hooks activate)
-  "Run `my-magit-after-stage-hooks` after staging an item in magit."
-  (when (called-interactively-p 'interactive)
-    (run-hooks 'my-magit-after-stage-hooks)))
-
-(defadvice magit-unstage-item (after run-my-after-unstage-hooks activate)
-  "Run `my-magit-after-unstage-hooks` after unstaging an item in magit."
-  (when (called-interactively-p 'interactive)
-    (run-hooks 'my-magit-after-unstage-hooks)))
-
-(defun my-refresh-visible-git-gutter-buffers ()
-  "Refresh git-gutter-mode on all visible git-gutter-mode buffers."
-  (dolist (buff (buffer-list))
-    (with-current-buffer buff
-      (when (and git-gutter-mode (get-buffer-window buff))
-        (git-gutter-mode t)))))
-
-(add-hook 'my-magit-after-unstage-hooks
-          'my-refresh-visible-git-gutter-buffers)
-(add-hook 'my-magit-after-stage-hooks
-          'my-refresh-visible-git-gutter-buffers)
 
 (provide 'init-helper)
