3a2d79e3ea70b23fe3258de8176bd8f6426a9da0
hiepph <hoanghiepjp96@gmail.com>
2017-11-25 01:24:21 +0700

    emacs: Neotree show hidden files

--- emacs/.emacs.d/lisp/init-ui.el
+++ emacs/.emacs.d/lisp/init-ui.el
@@ -38,13 +38,20 @@
 ; (all-the-icons-icon-for-buffer)
 ; (all-the-icons-icon-for-file)
 ; (all-the-icons-icon-for-mode)
+
 ;; slow rendering
 (setq inhibit-compacting-font-caches t)
+
 ;; set icons theme
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
 ;; Every time when the neotree window is opened, let it find current file and jump to node
 (setq neo-smart-open t)
+
 ;; When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically
 (setq projectile-switch-project-action 'neotree-projectile-action)
+
+;; show hidden files
+(setq-default neo-show-hidden-files t)
 
 (provide 'init-ui)
