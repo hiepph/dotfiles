6c37b769c9eb2b819b3290996d41d61affe63e42
Hiep Pham <hoanghiepjp96@gmail.com>
2018-01-11 18:14:21 +0700

    emacs: swap isearch and isearch-regex

--- emacs/.emacs.d/lisp/init-editing.el
+++ emacs/.emacs.d/lisp/init-editing.el
@@ -53,12 +53,12 @@
 ;; Highlight current line
 (global-hl-line-mode 1)
 
-;; Interactive search key bindings. By default, C-s runs
-;; isearch-forward, so this swaps the bindings.
-; (global-set-key (kbd "C-s") 'isearch-forward-regexp)
-; (global-set-key (kbd "C-r") 'isearch-backward-regexp)
-; (global-set-key (kbd "C-M-s") 'isearch-forward)
-; (global-set-key (kbd "C-M-r") 'isearch-backward)
+;; Interactive search key bindings.
+;; By default, C-s runs isearch-forward, so this swaps the bindings
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
 
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
