6985e914a5a78a6d231648b8f5e5701d2f51a1a0
Hiep Pham <hoanghiepjp96@gmail.com>
2018-02-20 17:35:44 +0700

    emacs: remove ripgrep + add helm with RG support

--- emacs/.emacs.d/lisp/init-search.el
+++ emacs/.emacs.d/lisp/init-search.el
@@ -1,5 +1,16 @@
 (require 'init-elpa)
 
-(require-package 'ripgrep)
+(require-package 'helm)
+
+;; Enable Helm mode
+(helm-mode 1)
+;; Set Helm to use `ripgrep`
+(setq helm-grep-ag-command "rg --color=always --colors 'match:fg:black' --colors 'match:bg:yellow' --smart-case --no-heading --line-number %s %s %s")
+(setq helm-grep-ag-pipe-cmd-switches '("--colors 'match:fg:black'" "--colors 'match:bg:yellow'"))
+;; Replace tab-completion
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+
 
 (provide 'init-search)
