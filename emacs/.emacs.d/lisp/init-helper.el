39725b042b026917645a54873294d486d629683d
Hiep Pham <hoanghiepjp96@gmail.com>
2018-02-22 11:32:49 +0700

    emacs: add TODO, FIXME, etc. highlight mode

--- emacs/.emacs.d/lisp/init-helper.el
+++ emacs/.emacs.d/lisp/init-helper.el
@@ -2,15 +2,27 @@
 
 (require-package 'git-gutter+)
 (require-package 'magit)
+(require-package 'hl-todo)
 
-; Magit
+
+;; Magit
 ;; git status
 (global-set-key (kbd "C-x g") 'magit-status)
 ;; shortcuts help
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
-; Git Gutter (+)
+
+;; Git Gutter (+)
 ;; Enable git-gutter on all mode
 (global-git-gutter+-mode)
+
+
+;; Enable highlighting TODO, FIXME, etc.
+(global-hl-todo-mode)
+;; Jump to next/prev phrases
+(define-key hl-todo-mode-map (kbd "C-c p") 'hl-todo-previous)
+(define-key hl-todo-mode-map (kbd "C-c n") 'hl-todo-next)
+(define-key hl-todo-mode-map (kbd "C-c o") 'hl-todo-occur)
+
 
 (provide 'init-helper)
