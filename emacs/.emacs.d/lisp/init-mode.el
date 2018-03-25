2a25887471bdab68c68d7c91f8de97441bf2fdc8
hiepph <hoanghiepjp96@gmail.com>
2018-03-26 01:35:41 +0700

    emacs: basic setup for LaTeX

--- emacs/.emacs.d/lisp/init-mode.el
+++ emacs/.emacs.d/lisp/init-mode.el
@@ -3,6 +3,7 @@
 (require-package 'org)
 (require-package 'markdown-mode)
 
+
 ;; Org-mode
 (add-hook 'org-mode-hook 'turn-on-font-lock)
 
@@ -20,6 +21,36 @@
 ;; For Markdown preview
 ;; npm install -g livedown
 ;; livedown start README.md --open
+
+
+;; #### LaTeX #####
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-save-query nil)
+(setq TeX-PDF-mode t) ;; .pdf statt .dvi per default:
+
+;; Setup for AUCTeX
+(require 'tex-site)
+(autoload 'reftex-mode "reftex" "RefTeX Minor Mode" t)
+(autoload 'turn-on-reftex "reftex" "RefTeX Minor Mode" nil)
+(autoload 'reftex-citation "reftex-cite" "Make citation" nil)
+(autoload 'reftex-index-phrase-mode "reftex-index" "Phrase Mode" t)
+(add-hook 'latex-mode-hook 'turn-on-reftex) ; with Emacs latex mode
+(add-hook 'reftex-load-hook 'imenu-add-menubar-index)
+
+;;Syntax Higlight
+(add-hook 'LaTeX-mode-hook 'turn-on-font-lock)
+;; Mathe Modus
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+;; PDF viewer
+(setq TeX-view-program-selection
+      '((output-pdf "PDF Viewer")))
+(setq TeX-view-program-list
+      '(("PDF Viewer" "okular %o")))
+
+;; Realtime preview
+;; latexmk -pvc -pdf file.tex
 
 
 (provide 'init-mode)
