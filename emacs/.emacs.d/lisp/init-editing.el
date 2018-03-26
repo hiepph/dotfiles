14251b30b815fcdeaee50065535e1bb941ad4fce
Hiep Pham <hoanghiepjp96@gmail.com>
2018-03-26 16:54:20 +0700

    emacs: C-% for going to matching parenthesis

--- emacs/.emacs.d/lisp/init-editing.el
+++ emacs/.emacs.d/lisp/init-editing.el
@@ -168,4 +168,14 @@
 ;; Redo
 (global-set-key (kbd "C-S-/") 'redo)
 
+
+;; Go to matching parenthesis
+(defun goto-match-paren (arg)
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+(global-set-key (kbd "C-%") 'goto-match-paren)
+
+
 (provide 'init-editing)
