dfbf086f9e56b013d29f7ccdb12f9f2d06c59ca1
hiepph <hoanghiepjp96@gmail.com>
2018-04-14 16:04:18 +0700

    emacs: Helm power-up

--- emacs/.emacs.d/lisp/init-navigation.el
+++ emacs/.emacs.d/lisp/init-navigation.el
@@ -1,15 +1,30 @@
 (require 'init-elpa)
-(require 'ido)
-(require 'recentf)
-(require-package 'ido-ubiquitous)
-(require-package 'smex)
+
+
+;; Projectile
 (require-package 'projectile)
+(projectile-global-mode)
+
+;; Power-up by helm
+(require-package 'helm-projectile)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+
+
+;; Recent files
+(require 'recentf)
 
 (setq recentf-save-file (concat user-emacs-directory ".recentf"))
 (recentf-mode 1)
 (setq recentf-max-menu-items 40)
 
+
+;; IDO
+(require 'ido)
+(require-package 'ido-ubiquitous)
+(require-package 'smex)
 (ido-mode t)
+
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length -1)
@@ -24,9 +39,9 @@
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 
-(projectile-global-mode)
 
 ;; Enable move point from window to window using Shift and the arrow keys
 (windmove-default-keybindings)
+
 
 (provide 'init-navigation)
