9fe275cb71d6af7c8e1e6a0e76f185d7e5299944
hiepph <hiepph.2406@gmail.com>
2018-11-06 16:44:06 +0700

    emacs: close trivial buffers after committing Magit

--- emacs/.emacs.d/lisp/init-helper.el
+++ emacs/.emacs.d/lisp/init-helper.el
@@ -2,12 +2,26 @@
 
 
 ;; Magit
+(defun kill-magit-diff-buffer-in-current-repo (&rest _)
+  "Delete the magit-diff buffer related to the current repo"
+  (let ((magit-diff-buffer-in-current-repo
+         (magit-mode-get-buffer 'magit-diff-mode)))
+    (kill-buffer magit-diff-buffer-in-current-repo)))
+
 (use-package magit
   :ensure t
   :bind (;; git status
          ("C-x g" . 'magit-status)
          ;; shortcuts help
-         ("C-x M-g" . 'magit-dispatch-popup)))
+         ("C-x M-g" . 'magit-dispatch-popup))
+  :config
+  ;; When 'C-c C-c' is pressed in the magit commit message buffer,
+  ;; delete the magit-diff buffer related to the current repo.
+  (add-hook 'git-commit-setup-hook
+            (lambda ()
+              (add-hook 'with-editor-post-finish-hook
+                        #'kill-magit-diff-buffer-in-current-repo
+                        nil t))))
 
 
 ;; Git Gutter (+)
