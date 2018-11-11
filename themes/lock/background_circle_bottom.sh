8d372cbcc70d5518822afacfa71f8bce41f8c6ce
Hiep Pham <hiepph.2406@gmail.com>
2018-11-11 19:43:46 +0700

    themes: add i3lock-color background_circle_bottom theme

---
+++ themes/lock/background_circle_bottom.sh
@@ -0,0 +1,6 @@
+i3lock -n -i ~/Pictures/lock/witcher.jpg  \
+    --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
+    --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
+    --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
+    --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+86:y+1003" \
+    --radius=15 --veriftext="" --wrongtext=""
