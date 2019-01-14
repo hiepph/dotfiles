429348a6234bc0886150d4c8c20cdbb3ce064b9c
hiepph <hiepph.2406@gmail.com>
2019-01-14 12:38:45 +0700

    emacs: change neotree to treemacs

--- emacs/.emacs.d/lisp/init-ui.el
+++ emacs/.emacs.d/lisp/init-ui.el
@@ -51,31 +51,75 @@
 
 
 ;; Tree
-(use-package neotree
+(use-package treemacs
   :ensure t
-  :bind ("<f8>" . 'neotree-toggle)
+  :defer t
   :init
-  ;; pull all fonts/icons
-  ;; (all-the-icons-install-fonts)
-  ;; enable icons
-  ;; (all-the-icons-icon-for-buffer)
-  ;; (all-the-icons-icon-for-file)
-  ;; (all-the-icons-icon-for-mode)
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
+          treemacs-deferred-git-apply-delay   0.5
+          treemacs-display-in-side-window     t
+          treemacs-file-event-delay           5000
+          treemacs-file-follow-delay          0.2
+          treemacs-follow-after-init          t
+          treemacs-follow-recenter-distance   0.1
+          treemacs-git-command-pipe           ""
+          treemacs-goto-tag-strategy          'refetch-index
+          treemacs-indentation                2
+          treemacs-indentation-string         " "
+          treemacs-is-never-other-window      nil
+          treemacs-max-git-entries            5000
+          treemacs-no-png-images              nil
+          treemacs-no-delete-other-windows    t
+          treemacs-project-follow-cleanup     nil
+          treemacs-persist-file               (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-recenter-after-file-follow nil
+          treemacs-recenter-after-tag-follow  nil
+          treemacs-show-cursor                t
+          treemacs-show-hidden-files          t
+          treemacs-silent-filewatch           nil
+          treemacs-silent-refresh             nil
+          treemacs-sorting                    'alphabetic-desc
+          treemacs-space-between-root-nodes   t
+          treemacs-tag-follow-cleanup         t
+          treemacs-tag-follow-delay           1.5
+          treemacs-width                      35)
 
-  ;; slow rendering
-  (setq inhibit-compacting-font-caches t)
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
 
-  ;; set icons theme
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null (executable-find "python3"))))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("<f8>" . treemacs-projectile)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
 
-  ;; Every time when the neotree window is opened, let it find current file and jump to node
-  (setq neo-smart-open t)
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
 
-  ;; When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically
-  (setq projectile-switch-project-action 'neotree-projectile-action)
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
 
-  ;; show hidden files
-  (setq-default neo-show-hidden-files t))
 
 ;; indent guide
 (use-package indent-guide
