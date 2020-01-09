2ce34a324fc390247d2b1ea9d5b83adc5c4c01d9
hiepph <hiepph.2406@gmail.com>
2020-01-09 14:18:42 +0700

    emacs: new control for multiple cursor

--- emacs/.emacs.d/lisp/init-keys.el
+++ emacs/.emacs.d/lisp/init-keys.el
@@ -51,27 +51,39 @@ Text editing
 "
   ("j" crux-top-join-line "join lines" :column "Text")
   ("k" crux-kill-whole-line "kill whole line")
-
   ("e" er/expand-region "expand-region" :column "Pair")
   ("d" sp-splice-sexp "delete surround")
-  ("r" sp-rewrap-sexp "rewrap surround")
+  ("r" sp-rewrap-sexp "rewrap")
   ("f" sp-forward-sexp "forward to closed wrapping")
   ("b" sp-backward-sexp "backward to open wrapping")
-
   ("q" nil "quit" :column nil)
   )
 
 
 (defhydra hydra-multiple-cursors (:color red :idle 2)
   "
-Multiple cursors
-----------------
-"
-  ;; ("c" mc/edit-lines "edit lines")
-  ("f" mc/mark-next-like-this "next")
-  ("b" mc/mark-previous-like-this "prev")
-  ("d" mc/mark-all-like-this "all")
-  )
+ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]   Next     [_n_]   Next     [_l_] Edit lines  [_0_] Insert numbers
+ [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
+ [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search
+ [Click] Cursor at point       [_q_] Quit"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("s" mc/mark-all-in-region-regexp :exit t)
+  ("0" mc/insert-numbers :exit t)
+  ("A" mc/insert-letters :exit t)
+  ("<mouse-1>" mc/add-cursor-on-click)
+  ;; Help with click recognition in this hydra
+  ("<down-mouse-1>" ignore)
+  ("<drag-mouse-1>" ignore)
+  ("q" nil))
 
 
 (defhydra hydra-files (:color blue :idle 2)
@@ -81,7 +93,6 @@ Files manipulation
 "
   ("D" crux-delete-file-and-buffer "Delete file and buffer")
   ("R" crux-rename-file-and-buffer "Rename file and buffer")
-
   ("q" nil "quit" :column nil)
   )
 
