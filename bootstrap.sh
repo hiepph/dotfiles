9160e89546dde6c639e2174862d01addc2b17343
Hiep Pham <hiepph.2406@gmail.com>
2020-02-06 10:55:11 +0000

    i3: simplify with personal config and i3blocks

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+# Bootstrap the whole newly installed Arch system
+# echo "$@"
+
+# Git
+git config --global user.email "hiepph.2406@gmail.com"
+git config --global user.name "Hiep Pham"
+git config --global core.editor "vim"
