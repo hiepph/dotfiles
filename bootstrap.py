ce0fa98262ac4f807fc4bc0f43526899582b9367
Hiep <hiepph.2406@gmail.com>
2020-02-10 14:03:33 +0700

    bootstrap: add font config

--- bootstrap.py
+++ bootstrap.py
@@ -64,12 +64,20 @@ def bootstrap_fcitx():
     """)
 
 
+def bootstrap_font():
+    execute(f"""
+    {_pacman} powerline powerline-fonts
+    sudo localedef -f UTF-8 -i en_US en_US.UTF-8
+    """)
+
+
 M = {'git': bootstrap_git,
      'nvidia': bootstrap_nvidia,
      'aur': bootstrap_aur,
      'vim': bootstrap_vim,
      'tmux': bootstrap_tmux,
      'fcitx': bootstrap_fcitx,
+     'font': bootstrap_font,
     }
 
 
