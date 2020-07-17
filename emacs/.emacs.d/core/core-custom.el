c5943cfbf07e27521618174653b72dda0da70f43
Hiep <hiepph.2406@gmail.com>
2020-07-17 11:40:01 +0700

    emacs: remove core-custom

--- emacs/.emacs.d/core/core-custom.el
+++
@@ -1,8 +0,0 @@
-(use-package gruvbox-theme)
-(load-theme 'gruvbox t)
-
-(set-frame-font "Input Mono" nil t)
-(add-to-list 'default-frame-alist
-             '(font . "Input Mono"))
-
-(provide 'core-custom)
