a53a0acac2bab672b0c900457d010f117671ca66
Hiep <hiepph.2406@gmail.com>
2020-10-19 10:42:58 +0700

    emacs: fix toggle keybinding for flyspell

--- emacs/.emacs.d/core/core-keybindings.el
+++ emacs/.emacs.d/core/core-keybindings.el
@@ -104,7 +104,7 @@
 (defhydra hydra-toggle (:columns 4 :exit t)
   ("f" #'format-mode "format") ;; off by default
   ("c" #'company-mode "company") ;; on by default
-  ("s" #'company-mode "flyspell") ;; on by default in text-mode
+  ("s" #'flyspell-mode "flyspell") ;; on by default in text-mode
   )
 
 ;;
