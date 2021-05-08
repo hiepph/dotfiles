09c2652ff414e86e90e6bca1c0e1d46bb9cd8f9e
Hiep Pham <hiepph.2406@gmail.com>
2021-05-09 00:10:28 +0700

    emacs: windmove for M-<arrow>

--- emacs/.emacs.d/core/core-keybindings.el
+++ emacs/.emacs.d/core/core-keybindings.el
@@ -188,11 +188,15 @@
   (global-set-key (kbd "M-9") 'eyebrowse-switch-to-window-config-9)
   (global-set-key (kbd "M-'") 'eyebrowse-last-window-config)
 
-
   (global-set-key (kbd "M-h") 'windmove-left)
   (global-set-key (kbd "M-l") 'windmove-right)
   (global-set-key (kbd "M-j") 'windmove-down)
   (global-set-key (kbd "M-k") 'windmove-up)
+
+  (global-set-key (kbd "<M-left>") 'windmove-left)
+  (global-set-key (kbd "<M-right>") 'windmove-right)
+  (global-set-key (kbd "<M-down>") 'windmove-down)
+  (global-set-key (kbd "<M-up>") 'windmove-up)
 
 
   ;;
