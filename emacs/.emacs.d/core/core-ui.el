45cbba5434b798404dc6fe308a9f364492af51cc
Hiep Pham <hiepph.2406@gmail.com>
2021-11-13 13:32:22 +0100

    emacs: beautify org mode

--- emacs/.emacs.d/core/core-ui.el
+++ emacs/.emacs.d/core/core-ui.el
@@ -23,6 +23,9 @@
 ;; Enable transient mark mode
 (transient-mark-mode 1)
 
+;; Text will fill to the full width of window
+(add-hook 'text-mode-hook 'visual-line-mode)
+
 ;;
 ;; Show column
 ;;
@@ -56,5 +59,68 @@
 (add-hook 'text-mode-hook
           (lambda ()
             (variable-pitch-mode 1)))
+
+;;
+;; Theme
+;;
+(straight-use-package
+ '(colorless-theme :type git
+                   :repo "https://git.sr.ht/~lthms/colorless-themes.el"
+                   :branch "master"))
+(load-theme 'einkless t)
+
+
+;;
+;; Fonts
+;;
+(set-frame-font "Input Mono" nil t)
+(add-to-list 'default-frame-alist '(font . "Input Mono"))
+
+;;
+;; ORG appearance
+;;
+
+;; Variable height for headings and list
+(let* ((variable-tuple
+          (cond ((x-list-fonts "ETBembo") '(:font "ETBembo"))
+                ((x-family-fonts "Input Sans") '(:font "Input Sans"))
+                (nil (warn "Cannot find a suitable font."))))
+         (base-font-color (face-foreground 'default nil 'default))
+         (headline `(:inherit default :weight bold :foreground ,base-font-color)))
+
+    (custom-theme-set-faces
+     'user
+     `(org-level-8 ((t (,@headline ,@variable-tuple))))
+     `(org-level-7 ((t (,@headline ,@variable-tuple))))
+     `(org-level-6 ((t (,@headline ,@variable-tuple))))
+     `(org-level-5 ((t (,@headline ,@variable-tuple))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.15))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.2))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.25))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
+
+;; Fancy font for reading Org
+(custom-theme-set-faces
+ 'user
+ '(variable-pitch ((t (:family "ETBembo" :height 160 :weight thin))))
+ '(fixed-pitch ((t (:family "Input Mono" :height 130 :weight regular))))
+ )
+
+;; At least I need to distinguish between text and code
+(custom-theme-set-faces
+ 'user
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
 
 (provide 'core-ui)
