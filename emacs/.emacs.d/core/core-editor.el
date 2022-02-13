f59d343091bab540f602fb326f72feb65eb2bcc7
Hiep Pham <hiepph.2406@gmail.com>
2022-02-13 10:00:10 +0100

    emacs: enable ANSI coloring for compilation buffer

--- emacs/.emacs.d/core/core-editor.el
+++ emacs/.emacs.d/core/core-editor.el
@@ -1,3 +1,6 @@
+(require 'ansi-color)
+
+
 ;; Disable tabs mode
 (setq-default indent-tabs-mode nil)
 
@@ -163,6 +166,15 @@
   (interactive)
   (save-buffer)
   (recompile))
+
+;;
+;; Enable ANSI coloring for compilation buffer
+;;
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
 
 ;;
