3986b40e4d9e79856686aca2503427122d28ed3f
Hiep Pham <hiepph.2406@gmail.com>
2022-02-25 12:01:48 +0100

    mac(hammerspoon): integrate clipboard tool

--- mac/.hammerspoon/init.lua
+++ mac/.hammerspoon/init.lua
@@ -1,11 +1,11 @@
-local super_shift = {"cmd", "shift"}
-local super_ctrl = {"cmd", "ctrl"}
-local super_alt = {"cmd", "alt"}
+local SUPER_SHIFT = {"cmd", "shift"}
+local SUPER_CTRL = {"cmd", "ctrl"}
+local SUPER_ALT = {"cmd", "alt"}
 
 --
 -- Window movement
 --
-hs.hotkey.bind(super_shift, "h", function()
+hs.hotkey.bind(SUPER_SHIFT, "h", function()
       local win = hs.window.focusedWindow()
       local f = win:frame()
       local screen = win:screen()
@@ -18,7 +18,7 @@ hs.hotkey.bind(super_shift, "h", function()
       win:setFrame(f)
 end)
 
-hs.hotkey.bind(super_shift, "l", function()
+hs.hotkey.bind(SUPER_SHIFT, "l", function()
       local win = hs.window.focusedWindow()
       local f = win:frame()
       local screen = win:screen()
@@ -31,7 +31,7 @@ hs.hotkey.bind(super_shift, "l", function()
       win:setFrame(f)
 end)
 
-hs.hotkey.bind(super_shift, "k", function()
+hs.hotkey.bind(SUPER_SHIFT, "k", function()
       local win = hs.window.focusedWindow()
       local f = win:frame()
       local screen = win:screen()
@@ -44,7 +44,7 @@ hs.hotkey.bind(super_shift, "k", function()
       win:setFrame(f)
 end)
 
-hs.hotkey.bind(super_shift, "j", function()
+hs.hotkey.bind(SUPER_SHIFT, "j", function()
       local win = hs.window.focusedWindow()
       local f = win:frame()
       local screen = win:screen()
@@ -57,7 +57,7 @@ hs.hotkey.bind(super_shift, "j", function()
       win:setFrame(f)
 end)
 
-hs.hotkey.bind(super_shift, "c", function()
+hs.hotkey.bind(SUPER_SHIFT, "c", function()
       local win = hs.window.focusedWindow()
       local screen = win:screen()
 
@@ -98,21 +98,21 @@ end)
 --
 -- Window size
 --
-hs.hotkey.bind(super_shift, "return", function()
+hs.hotkey.bind(SUPER_SHIFT, "return", function()
       local win = hs.window.focusedWindow()
       local screen = win:screen()
 
       win:maximize()
 end)
 
-hs.hotkey.bind(super_shift, "m", function()
+hs.hotkey.bind(SUPER_SHIFT, "m", function()
       local win = hs.window.focusedWindow()
       local screen = win:screen()
 
       win:minimize()
 end)
 
-hs.hotkey.bind(super_shift, "f", function()
+hs.hotkey.bind(SUPER_SHIFT, "f", function()
       local win = hs.window.focusedWindow()
       local screen = win:screen()
 
@@ -148,19 +148,19 @@ function resize(wPercent, hPercent)
 end
 
 local window_resize_ratio = 0.15
-hs.hotkey.bind(super_shift, '.', resize(window_resize_ratio, 0))
-hs.hotkey.bind(super_shift, ',', resize(-window_resize_ratio, 0))
-hs.hotkey.bind(super_shift, '=', resize(0, window_resize_ratio))
-hs.hotkey.bind(super_shift, '-', resize(0, -window_resize_ratio))
-hs.hotkey.bind(super_shift, ']', resize(window_resize_ratio, window_resize_ratio))
-hs.hotkey.bind(super_shift, '[', resize(-window_resize_ratio, -window_resize_ratio))
+hs.hotkey.bind(SUPER_SHIFT, '.', resize(window_resize_ratio, 0))
+hs.hotkey.bind(SUPER_SHIFT, ',', resize(-window_resize_ratio, 0))
+hs.hotkey.bind(SUPER_SHIFT, '=', resize(0, window_resize_ratio))
+hs.hotkey.bind(SUPER_SHIFT, '-', resize(0, -window_resize_ratio))
+hs.hotkey.bind(SUPER_SHIFT, ']', resize(window_resize_ratio, window_resize_ratio))
+hs.hotkey.bind(SUPER_SHIFT, '[', resize(-window_resize_ratio, -window_resize_ratio))
 
 --
 -- Window visibility
 --
 
 -- this equals clicking the red close button
-hs.hotkey.bind(super_shift, "x", function()
+hs.hotkey.bind(SUPER_SHIFT, "x", function()
       local win = hs.window.focusedWindow()
       win:close()
 end)
@@ -171,14 +171,14 @@ end)
 --
 
 -- Move window between screens
-hs.hotkey.bind(super_ctrl, "h", function()
+hs.hotkey.bind(SUPER_CTRL, "h", function()
       local win = hs.window.focusedWindow()
       local f = win:frame()
 
       win:moveOneScreenWest()
 end)
 
-hs.hotkey.bind(super_ctrl, "l", function()
+hs.hotkey.bind(SUPER_CTRL, "l", function()
       local win = hs.window.focusedWindow()
       local f = win:frame()
 
@@ -187,14 +187,14 @@ end)
 
 -- Scale
 -- Larger text (zoom-in effect)
-hs.hotkey.bind(super_ctrl, "=", function()
+hs.hotkey.bind(SUPER_CTRL, "=", function()
                    local screen = hs.screen.primaryScreen()
                    local cur = screen:currentMode()
                    screen:setMode(1280, 800, cur.scale, cur.freq, cur.depth)
 end)
 
 -- Smaller text (zoom-out effect)
-hs.hotkey.bind(super_ctrl, "-", function()
+hs.hotkey.bind(SUPER_CTRL, "-", function()
                    local screen = hs.screen.primaryScreen()
                    local cur = screen:currentMode()
                    screen:setMode(1440, 900, cur.scale, cur.freq, cur.depth)
@@ -205,7 +205,7 @@ end)
 --
 
 -- kill gracefully (close and kill)
-hs.hotkey.bind(super_shift, "q", function()
+hs.hotkey.bind(SUPER_SHIFT, "q", function()
       local win = hs.window.focusedWindow()
       win:application():kill()
 end)
@@ -213,7 +213,7 @@ end)
 --
 -- Lock
 --
-hs.hotkey.bind(super_shift, "escape", function()
+hs.hotkey.bind(SUPER_SHIFT, "escape", function()
         hs.caffeinate.lockScreen()
 end)
 
@@ -284,11 +284,26 @@ end tell
 ]], cd_command, exec_command))
 end)
 
+--
+-- Clipboard management
+-- ref: https://www.hammerspoon.org/Spoons/ClipboardTool.html
+--
+function setupClipboardTool()
+    tool = hs.loadSpoon("ClipboardTool")
+    tool.show_copied_alert = false
+    tool.paste_on_select = true
+    tool:start()
+    tool:bindHotkeys({
+            toggle_clipboard = {SUPER_ALT, "p"}
+    })
+end
+
+setupClipboardTool()
 
 --
 -- Hot reload
 --
-hs.hotkey.bind(super_shift, "r", function()
+hs.hotkey.bind(SUPER_SHIFT, "r", function()
       hs.reload()
 end)
 hs.alert.show("Config loaded")
