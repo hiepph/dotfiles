742d2a4556884ae4a23d2137dace172464aa844a
Hiep Pham <hiepph@tuta.io>
2022-06-17 00:33:43 +0200

    fish: add config.fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/hiepph/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/hiepph/Downloads/google-cloud-sdk/path.fish.inc'; end
+
+# Abbreviations
+abbr -a -g g 'git'
+abbr -a -g k 'kubectl'
