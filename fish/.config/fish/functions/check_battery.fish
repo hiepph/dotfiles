a5d88f90ee505e22c02472709d59a27edc5a76c3
Hiep Pham <hiepph@tuta.io>
2022-06-19 18:27:12 +0200

    fish: fix the condition to notify low-battery

--- fish/.config/fish/functions/check_battery.fish
+++ fish/.config/fish/functions/check_battery.fish
@@ -3,8 +3,7 @@ function check_battery
     set bat_status (cat $bat_files/status)
     set bat_capacity (cat $bat_files/capacity)
 
-    # if test $bat_status = "Discharging" && test $bat_capacity -le 15
-    if test $bat_status = "Charging" && test $bat_capacity -ge 15
+    if test $bat_status = "Discharging" && test $bat_capacity -le 15
         notify-send -u critical "Battery low: $bat_capacity%" -t 10000
     end
 end
