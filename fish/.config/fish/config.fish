ae3826dddfc4d486cf50d99a64ab376d3f7ed4fd
Hiep Pham <hiepph@tuta.io>
2022-06-19 16:35:38 +0200

    fish: source custom fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,8 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/hiepph/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/hiepph/Downloads/google-cloud-sdk/path.fish.inc'; end
+# Customs for each machine
+. ~/customs/tools.fish
 
 # Abbreviations
 abbr -a -g g 'git'
