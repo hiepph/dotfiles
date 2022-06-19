1a4c6bc89ddd66cd26481cf7c74f60381d92e663
Hiep Pham <hiepph@tuta.io>
2022-06-19 18:21:06 +0200

    fish: add check_battery function

---
+++ fish/.config/fish/functions/check_battery.fish
@@ -0,0 +1,10 @@
+function check_battery
+    set bat_files "/sys/class/power_supply/BAT0"
+    set bat_status (cat $bat_files/status)
+    set bat_capacity (cat $bat_files/capacity)
+
+    # if test $bat_status = "Discharging" && test $bat_capacity -le 15
+    if test $bat_status = "Charging" && test $bat_capacity -ge 15
+        notify-send -u critical "Battery low: $bat_capacity%" -t 10000
+    end
+end
