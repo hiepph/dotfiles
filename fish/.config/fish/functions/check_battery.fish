0209671971c6feb80398cb9c1e5b118da2eb1a1a
Hiep Pham <hiepph@tuta.io>
2022-07-16 11:05:44 +0200

    fish: add ,gitignore function

--- fish/.config/fish/functions/check_battery.fish
+++
@@ -1,9 +0,0 @@
-function check_battery
-    set bat_files "/sys/class/power_supply/BAT0"
-    set bat_status (cat $bat_files/status)
-    set bat_capacity (cat $bat_files/capacity)
-
-    if test $bat_status = "Discharging" && test $bat_capacity -le 15
-        notify-send -u critical "Battery low: $bat_capacity%" -t 10000
-    end
-end
