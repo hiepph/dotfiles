a6d47aa0293eea9c7496489b760aadc13041b0df
Hiep Pham <hiepph@tuta.io>
2022-08-26 10:39:31 +0200

    mac(nix): remove fish config

--- mac/.config/nixpkgs/main.nix
+++ mac/.config/nixpkgs/main.nix
@@ -3,27 +3,10 @@
 
 with pkgs;
 {
-  programs.fish = {
-    enable = false;
-
-    plugins = [
-      {
-        name = "fish-kubectl-completions";
-        src = pkgs.fetchFromGitHub {
-          owner = "evanlucas";
-          repo = "fish-kubectl-completions";
-          rev = "ced676392575d618d8b80b3895cdc3159be3f628";
-          sha256 = "09qcj82qfs4y4nfwvy90y10xmx6vc9yp33nmyk1mpvx0dx6ri21r";
-        };
-      }
-    ];
-  };
-
   home.packages = [
     #
     # dev
     #
-
     git
     git-secret
 
