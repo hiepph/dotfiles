feddf65d1ca95e47e26765761e4d1e22420b2990
Hiep Pham <hiep.pham@resourcify.de>
2023-01-18 15:06:58 +0100

    emacs: remove mood modeline

--- emacs/.emacs.d/core/core-ui.el
+++ emacs/.emacs.d/core/core-ui.el
@@ -143,16 +143,4 @@
   (global-yascroll-bar-mode 1))
 
 
-;;
-;; Mode line
-;; ref: https://github.com/tarsius/moody
-;;
-(use-package moody
-  :config
-  (setq x-underline-at-descent-line t)
-  (moody-replace-mode-line-buffer-identification)
-  (moody-replace-vc-mode)
-  (moody-replace-eldoc-minibuffer-message-function))
-
-
 (provide 'core-ui)
