feba3211722f80a43fa0914b3ce56d066c5a7110
Hiep Pham <hiepph@tuta.io>
2023-02-10 11:48:55 +0100

    emacs: replace company with corfu + cape

--- emacs/.emacs.d/core/core-editor.el
+++ emacs/.emacs.d/core/core-editor.el
@@ -161,14 +161,6 @@
 ;; on automatically for text-mode
 (add-hook 'text-mode-hook 'flyspell-mode)
 
-;;
-;; Company
-;;
-(use-package company
-  :config
-  ;; complete word with original cases
-  (setq company-dabbrev-downcase nil))
-
 
 ;;
 ;; Undo/Redo
@@ -274,5 +266,33 @@ Supports MacOS (with Hammerspoon) for now."
   (interactive "M$: ")
   (shell-command
    (format "open -g 'hammerspoon://terminal?dir=%s&command=%s'" default-directory command)))
+
+
+;;
+;; Completion at point extensions
+;; ref: https://github.com/minad/cape
+;; ref: https://github.com/minad/corfu
+;;
+(use-package corfu
+  :init
+  (global-corfu-mode))
+
+(use-package cape
+  :init
+  ;; Add `completion-at-point-functions', used by `completion-at-point'.
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-history)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  (add-to-list 'completion-at-point-functions #'cape-line)
+  (add-to-list 'completion-at-point-functions #'cape-ispell)
+  ;;(add-to-list 'completion-at-point-functions #'cape-tex)
+  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+  ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
+  )
+
 
 (provide 'core-editor)
