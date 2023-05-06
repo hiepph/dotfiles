9cc3b4e2a48dc32272cce4d250be56b4cea95516
Hiep Pham <hiepph@tuta.io>
2023-05-06 11:29:49 +0200

    refactor: manage sqlite config with home-manager

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -77,10 +77,10 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # Building this configuration will create a copy of 'sqlite/.sqliterc' in
+    # # the Nix store. Activating the configuration will then make '~/.sqliterc' a
     # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    ".sqliterc".source = sqlite/.sqliterc;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
