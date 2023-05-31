b955ccabefd3d4aa7d72162320b1b5918ba0b51c
Hiep Pham <hiepph@tuta.io>
2023-05-31 12:06:28 +0200

    feat(scripts): add ,stopwatch utility

--- fish/.config/fish/functions/,stopwatch.fish
+++
@@ -1,8 +0,0 @@
-function ,stopwatch --description "Start a stopwatch"
-    set start (date +%s)
-    while true
-        set time (math (date +%s) - $start)
-        printf '%s\r ' (date -r $time +"%H:%M:%S")
-        sleep .5
-    end
-end
