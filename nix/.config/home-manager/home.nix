44056ec84f4d55f36e9128c7bedfd7b04040bd35
Hiep Pham <hiepph@tuta.io>
2023-06-14 23:58:42 +0200

    feat(nix): add `lf` tool

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -64,6 +64,7 @@ in
     pkgs.jo
     pkgs.wget
     pkgs.htop
+    pkgs.lf # terminal file manager, ranger-inspired
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
