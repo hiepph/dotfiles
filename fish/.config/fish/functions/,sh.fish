7d8033b4ae507e0124e644ecd04f5814f4042254
Hiep Pham <hiepph@tuta.io>
2023-07-23 00:54:08 +0200

    feat(fish): ,sh exit with command exit code

--- fish/.config/fish/functions/,sh.fish
+++ fish/.config/fish/functions/,sh.fish
@@ -1,4 +1,26 @@
+function log_error
+    echo -e "\t> $argv[1]" >&2
+end
+
+
 function ,sh --description "Execute a command, verbosely"
     echo -e "\t\$ $argv"
-    command $argv[1..-1]
+
+    set tmp_dir (mktemp -d)
+    set err_log $tmp_dir/,sh.err
+
+    $argv[1..-1] 2>$err_log
+    set code $status
+
+    if test $status -ne 0
+        log_error "Command failed: \"$argv[1..-1]\" with status code: $code"
+
+        log_error "STDERR: "
+        cat $err_log >&2
+        log_error ""
+
+        exit $code
+    end
+
+    echo ""
 end
