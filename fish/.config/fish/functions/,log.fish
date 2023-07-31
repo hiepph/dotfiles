cc3c957cd3720e34f2b6c876fa3c17510bc08b4c
Hiep Pham <hiepph@tuta.io>
2023-08-01 00:33:46 +0200

    feat(fish): ,log with --error option

--- fish/.config/fish/functions/,log.fish
+++ fish/.config/fish/functions/,log.fish
@@ -1,5 +1,25 @@
-#!/usr/bin/env fish
-
+#
+# USAGE:
+#
+# - To write normally to stdout:
+#
+#   $ ,log "The program ran well."
+#
+#   [OUT]> The program ran well.
+#
+# - To write to stderr:
+#
+#   $ ,log -e "Something happened!"
+#   $ ,log --error "Something happend!"
+#
+#   [ERR]> Error
+#
 function ,log --description "Log a message to the stderr"
-    echo -e "\t> $argv[1..]" >&2
+    argparse --ignore-unknown 'e/error'  -- $argv
+
+    if set -q _flag_error
+        echo -e "\tERR> $argv[1..]" >&2
+    else
+        echo -e "\tOUT> $argv[1..]"
+    end
 end
