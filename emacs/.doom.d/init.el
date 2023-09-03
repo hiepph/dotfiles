9ac361138177bcefd28dffce09522b05accd9f0a
Hiep Pham <hiepph@tuta.io>
2023-09-03 16:12:32 +0200

    feat(emacs): add ivy

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -22,7 +22,7 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;ivy               ; a search engine for love and life
+       (ivy +prescient +childframe +icons)               ; a search engine for love and life
        vertico           ; the search engine of the future
 
        :ui
@@ -30,7 +30,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; 🙂
+       (emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
