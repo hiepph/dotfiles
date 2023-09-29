f20a3291a57b5392be4eacc6bda2658f259398aa
Hiep Pham <hiepph9@proton.me>
2023-09-29 23:25:07 +0200

    docs: add uBlock Origin guide

--- macports/install_ports.sh
+++ macports/install_ports.sh
@@ -1,5 +1,22 @@
+#
+# shell
+#
 sudo port install fish
+
+#
+# text editors
+#
+
 # refer: https://ports.macports.org/port/emacs-app/details/
 sudo port install emacs-app +nativecomp +treesitter
-# sudo port install rbenv
-sudo port install pinentry-mac
+
+#
+# languages
+#
+
+# sudo port install rbenv ruby-build
+
+#
+# ops
+#
+sudo port install pulumi
