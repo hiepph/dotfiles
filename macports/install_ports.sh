4ff54d498cb59c2bc2f0acdf5897e3abe24ddd1c
Hiep Pham <hiepph9@proton.me>
2023-09-29 20:40:39 +0200

    feat: use MacPorts instead of Homebrew

---
+++ macports/install_ports.sh
@@ -0,0 +1,5 @@
+sudo port install fish
+# refer: https://ports.macports.org/port/emacs-app/details/
+sudo port install emacs-app +nativecomp +treesitter
+# sudo port install rbenv
+sudo port install pinentry-mac
