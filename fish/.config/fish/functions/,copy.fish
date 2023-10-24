ee879780375568a36e95658824c3404162ae155d
Hiep Pham <hiepph9@proton.me>
2023-10-24 11:29:29 +0200

    refactor: move ,copy to scripts

--- fish/.config/fish/functions/,copy.fish
+++
@@ -1,8 +0,0 @@
-function ,copy --description "Copy the content to clipboard"
-    switch (uname)
-        case Darwin
-            pbcopy
-        case Linux
-            xsel --clipboard
-    end
-end
