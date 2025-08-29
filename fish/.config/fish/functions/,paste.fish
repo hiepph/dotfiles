c6ea17c760c80d00f8b3eacbaef4be8087d6f41e
Hiep Pham <hiepph9@proton.me>
2025-08-29 15:16:09 +0200

    fish: update ,paste script location

--- fish/.config/fish/functions/,paste.fish
+++
@@ -1,8 +0,0 @@
-function ,paste --description "Paste the clipboard content"
-    switch (uname)
-        case Darwin
-            pbpaste
-        case Linux
-            xsel --clipboard --output
-    end
-end
